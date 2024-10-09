01dffb60a694c14b8834c1b691a9576d0a1f1b6e
Евгений <info-best@bk.ru>
2024-10-09 19:27:35 +0300

    second

--- module_9_6.py
+++ module_9_6.py
@@ -1,24 +1,23 @@
 def all_variants(text):
-    while True:
-        yield text[0]
-        yield text[1]
-        yield text[2]
-        break
-    while True:
-        yield text[0] + text[1]
-        yield text[1] + text[2]
-        break
-    yield text[0] + text[1] + text[2]
+    # while True:
+    #     yield text[0]
+    #     yield text[1]
+    #     yield text[2]
+    #     break
+    # while True:
+    #     yield text[0] + text[1]
+    #     yield text[1] + text[2]
+    #     break
+    # yield text[0] + text[1] + text[2]
 
-    # for i in text:
-    #     if i in text:
-    #         yield i
-    #
-    #     if i == text[0]:
-    #         yield i + text[1]
-    #     if i == text[1]:
-    #         yield i + text[2]
-    #     yield text
+    for i in text:
+        if i in text:
+            yield i
+    for j in text:
+        if j == text[0]:
+            yield j + text[1]
+            yield text[1] + text[2]
+    yield text
 
 
 
@@ -26,5 +25,12 @@ def all_variants(text):
 
 
 a = all_variants("abc")
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# print(next(a))
 for i in a:
     print(i)
+
